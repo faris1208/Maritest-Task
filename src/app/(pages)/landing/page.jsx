2884314f2ab0b9de54a749f293a92dbe45efc230
faris1208 <farislawal3@gmail.com>
2024-07-14 08:26:56 -0700

    commit

---
+++ src/app/(pages)/landing/page.jsx
@@ -0,0 +1,10 @@
+import Landing from '@/app/components/landing'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        <Landing />
+    </div>
+  )
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import styles from "./page.module.scss";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+   <h2>wduwdueduwe</h2>
+   <h2>wduwdueduwe</h2>
+   <h2>wduwdueduwe</h2>
+   <h2>wduwdueduwe</h2>
+   <h2>wduwdueduwe</h2>
+   <h2>wduwdueduwe</h2>
+   <h2>wduwdueduwe</h2>
+   <h2>wduwdueduwe</h2>
+   <h2>wduwdueduwe</h2>
+   <h2>wduwdueduwe</h2>
+    </main>
+  );
+}
